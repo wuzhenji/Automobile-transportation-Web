0985ef740072b7712cd9cf23a3237944bb440775
wuzhenji <15195035689@163.com>
2021-09-01 11:06:08 +0800

    'init'

---
+++ src/layouts/BasisLayout/index.tsx
@@ -0,0 +1,10 @@
+import styles from './index.less';
+import React from 'react';
+import {} from 'umi';
+import {} from 'antd';
+
+const BasisLayout: React.FC = () => {
+  return <div className={styles.BasisLayout}>BasisLayout</div>;
+};
+
+export default BasisLayout;
